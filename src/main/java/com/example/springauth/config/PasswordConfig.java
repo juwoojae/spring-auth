fda36612ef54beef023c3748aa147d12a96af450
Juwoojae <juwqq1234@naver.com>
2025-10-22 12:13:33 +0900

    Initial commit

---
+++ src/main/java/com/example/springauth/config/PasswordConfig.java
@@ -0,0 +1,18 @@
+package com.example.springauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    /**
+     * 스프링 빈 수동등록
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //해시함수 (비밀번호를 암호화하는)
+    }
+}
